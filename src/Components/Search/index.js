b063202f28e839cc26d6719108d256bf90651760
Aruturuu <artigames666@gmail.com>
2021-06-11 03:25:24 +0200

    Revert "Revert "rrtfyfdg""
    This reverts commit a93910153f47c077717c4c0f133ec9e321ae1d8f.

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,4 +1,5 @@
 import navBar from "../navbar.js";
+<<<<<<< Updated upstream
 import "./styles.css";
 
 export default function Category() {
@@ -16,3 +17,45 @@ export default function Category() {
     </>
   );
 }
+=======
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { AuthContext } from "../../Context/authContext";
+import "./styles.css";
+
+export default function Category() {
+    const [search, setSearch] = useState([]);
+    const { query } = useParams();
+
+    function searchResult() {
+        return search.map((element) => {
+            return (
+                <Link to={`../Listing/${element.listingId}`} className="search-result-module">
+                    {element.name}
+                </Link>
+            );
+        });
+    }
+
+    useEffect(() => {
+        fetch(
+            `https://moderato-backend.herokuapp.com/api/v1/listings/search/${query}`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setSearch(data);
+            });
+    }, []);
+
+    return (
+        <>
+            {navBar()}
+            <div className="search-main">
+                <labe>Searched listings:</labe>
+                {searchResult()}
+            </div>
+        </>
+    );
+}
+>>>>>>> Stashed changes
